fd061194116f6f887167e61615f1caca5785422b
callmefisher <xiayanjiji@gmail.com>
2018-02-23 11:23:50 +0800

    machine learing excercise 2 compute Cost function

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -7,16 +7,27 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+Transtheta = theta';
+ThetaLen = length(Transtheta);
+FinalSum = 0;
+DesignX = X';
+%%[XColume, XLine] = size(DesignX);
+%%[ThetaColm, ThetaLine] = size(Transtheta);
+%%fprintf("特征矩阵X (Colume:%d  Line:%d), 样本数量:%d Theta矩阵(Colume:%d  Line:%d)\n", XColume, XLine, m, ThetaColm, ThetaLine);
+for i=1:m
+   TmpInnerSum = 0;
+   for j = 1:ThetaLen
+    TmpInnerSum = TmpInnerSum + Transtheta(1,j)* DesignX(j,i);
+   end
+    TmpReduceNum = TmpInnerSum - y(i,1);
+    FinalSum = FinalSum +  TmpReduceNum * TmpReduceNum;
+end
 
-
-
-
-
+J = FinalSum / (2 * m);
 % =========================================================================
 
 end
