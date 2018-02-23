cd15086222c53546c3350d7635e225fc0cd689ca
callmefisher <xiayanjiji@gmail.com>
2018-02-23 13:38:04 +0800

    add pause in matlab script

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -63,7 +63,7 @@ fprintf('\nTesting the cost function ...\n')
 J = computeCost(X, y, theta);
 fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
 fprintf('Expected cost value (approx) 32.07\n');
-
+pause;
 % further testing of the cost function
 J = computeCost(X, y, [-1 ; 2]);
 fprintf('\nWith theta = [-1 ; 2]\nCost computed = %f\n', J);
