34901b8ec2a3a9b567c04170a135dc8a530d7090
callmefisher <xiayanjiji@gmail.com>
2018-02-23 11:48:25 +0800

    add ignore rule

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -2,7 +2,7 @@ function J = computeCost(X, y, theta)
 %COMPUTECOST Compute cost for linear regression
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
-
+slCharacterEncoding('UTF-8');
 % Initialize some useful values
 m = length(y); % number of training examples
 
