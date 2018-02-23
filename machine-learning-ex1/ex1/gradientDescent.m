a6b97f9829621897db356ad41fbc4fd08854d002
callmefisher <xiayanjiji@gmail.com>
2018-02-23 15:58:19 +0800

    gradent descent exercise

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,9 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+[ThetaColum, ThetaLine] = size(theta);
+
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,7 +19,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    for j = 1:
 
 
 
